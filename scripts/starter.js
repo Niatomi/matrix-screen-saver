54b7180953819cb106d2c8437082095697a25b88
Niatomi <playervoker@gmail.com>
2022-01-06 03:31:12 +0400

    Comment of setInterval for add td element

---
+++ scripts/starter.js
@@ -0,0 +1,16 @@
+const charToChoose = ["1", "2", "3", "4", "5", "6"];
+
+function getRandomBetween(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+// elem.innerHTML = " my Text ";
+
+// setInterval(() => {
+//   let newRow = document.createElement("tr");
+//   newRow.id = "row";
+//   setInterval(() => {
+
+//   }, 200);
+//   document.body.insertAfter(newRow, document.body.after("tbody"));
+// }, 10000);
